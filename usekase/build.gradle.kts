c72fc91d3447ef80d4def101679de155a53abfd9
StefMa <StefMaDev@outlook.com>
2018-04-03 17:15:45 +0200

    Fix tests

--- usekase/build.gradle.kts
+++ usekase/build.gradle.kts
@@ -11,10 +11,12 @@ dependencies {
     compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jre7:1.2.31")
     compileOnly("io.reactivex.rxjava2:rxjava:2.1.12")
 
+    testCompile("org.jetbrains.kotlin:kotlin-stdlib-jre7:1.2.31")
+    testCompile("io.reactivex.rxjava2:rxjava:2.1.12")
+
     testRuntime("org.junit.jupiter:junit-jupiter-engine:5.1.0")
     testCompile("org.junit.jupiter:junit-jupiter-api:5.1.0")
     testCompile("org.assertj:assertj-core:3.9.0")
-    testCompile("io.reactivex.rxjava2:rxjava:2.1.12")
 }
 
 tasks.withType<Test> {
