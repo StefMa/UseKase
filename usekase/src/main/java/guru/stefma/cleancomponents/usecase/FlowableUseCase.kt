47c53cd8db9d77985ca18524e5f000adf55a07c8
StefMa <StefMaDev@outlook.com>
2018-08-02 09:34:36 +0200

    Create FlowableUseCase

---
+++ usekase/src/main/java/guru/stefma/cleancomponents/usecase/FlowableUseCase.kt
@@ -0,0 +1,19 @@
+package guru.stefma.cleancomponents.usecase
+
+import io.reactivex.Flowable
+
+/**
+ * A extension over the generic [RxUseCase] which wrap a [result][R] into a [Flowable] instance.
+ *
+ * It will use the provided [executionScheduler] and [postExecutionScheduler] as
+ * [Flowable.subscribeOn] and [Flowable.observeOn].
+ *
+ * @param P the params you want to put in to the UseCase
+ * @param R the result value which will be emitted by this UseCase
+ */
+interface FlowableUseCase<R, P> : RxUseCase<Flowable<R>, P> {
+
+    override fun execute(params: P): Flowable<R> = buildUseCase(params)
+            .subscribeOn(executionScheduler)
+            .observeOn(postExecutionScheduler)
+}
