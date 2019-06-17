0a41690f588ea68efd162cacec36e788e5f7d2eb
StefMa <StefMaDev@outlook.com>
2019-06-17 16:27:31 +0200

    Use same uploadName for each module
    ...otherwise a new package will be created on bintray

--- usekase-annotation/build.gradle.kts
+++ usekase-annotation/build.gradle.kts
@@ -15,7 +15,7 @@ publish {
     userOrg = "stefma"
     groupId = "guru.stefma.cleancomponents"
     artifactId = "usekase-annotation"
-    uploadName = "UseKase-Annotation"
+    uploadName = "UseKase"
     publishVersion = "1.0.0"
     desc = "Provides a UseCase annotation for the Clean Architecture"
     website = "https://github.com/StefMa/UseKase"
